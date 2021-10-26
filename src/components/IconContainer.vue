f587b01fbaf19e658cac6df46f905e49a5d090c7
Timur Rakhimov <trahimov@roox.ru>
2021-10-26 15:12:33 +0300

    Add IconContainer

---
+++ src/components/IconContainer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IconContainer',
+};
+</script>
+
+<style scoped>
+.container {
+  width: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
