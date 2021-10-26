f33d38f8008f8dc41dd4ca48e428938ae72e7e99
Timur Rakhimov <trahimov@roox.ru>
2021-10-26 15:13:02 +0300

    Add AppPath to App components

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,44 @@
       :node="treeData"
       :path="treeData.name"
     />
+    <app-path :path="selectedPath"/>
   </div>
 </template>
 
 <script>
 import treeData from '../public/static/node_modules.json';
+import AppPath from './components/AppPath.vue';
 import DirectoryItem from './components/DirectoryItem.vue';
 
 export default {
   name: 'App',
+  provide() {
+    return {
+      getSelectedPath: this.getSelectedPath,
+      setSelectedPath: this.setSelectedPath,
+    };
+  },
   data() {
     return {
       treeData,
+      selectedPath: '',
     };
   },
   components: {
     DirectoryItem,
+    AppPath,
   },
+  methods: {
+    setSelectedPath(newSelectedPath) {
+      let pathToSet = '';
+      if (this.selectedPath !== newSelectedPath) {
+        pathToSet = newSelectedPath;
+      }
+      this.selectedPath = pathToSet;
+    },
+    getSelectedPath() {
+      return this.selectedPath;
+    },
   },
 };
 </script>
---
+++ src/components/AppPath.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="container">
+    <div class="title">Path:</div>
+    <ul
+      class="list"
+      v-if="getSplitedPath"
+    >
+      <li
+        v-for="(pathItem, idx) in getSplitedPath"
+        :key="`${pathItem}-${idx}`"
+        class="list__item"
+       >
+        {{ pathItem }}
+        <font-awesome-icon
+          v-if="idx !== getSplitedPath.length - 1"
+          icon="angle-right"
+          class="list__item__separator"
+        />
+      </li>
+    </ul>
+    <span v-else>Nothing selected</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppPath',
+  props: {
+    path: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    getSplitedPath() {
+      return this.path && this.path.split('/');
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+}
+
+.title {
+  font-weight: bold;
+}
+
+.list {
+  list-style-type: none;
+  display: flex;
+  margin: 8px 0;
+  padding: 0;
+}
+
+.list__item {
+  display: flex;
+  align-items: center;
+}
+
+.list__item__separator {
+  padding: 0 6px;
+}
+
+</style>
